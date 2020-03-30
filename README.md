92e4dab6c5ac487ef89f982ab53228a470d9acc3
critcalfail <criticalfail@github.com>
2020-03-31 02:59:22 +0300

    finito

---
+++ README.md
@@ -0,0 +1,4 @@
+# Python Text Writer
+
+> Reads file then types after 5 sec 
+
--- test.py
+++ test.py
@@ -1,7 +1,11 @@
 from random import random
 import time
+from pynput.keyboard import Controller
+
+keyboard=Controller()
+time.sleep(5)
 with open("test.txt","r") as f:
     while True:
         c=f.read(1)
-        print(c)
+        keyboard.type(c)
         time.sleep(random()*0.5)
